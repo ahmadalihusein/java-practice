16b13ef1cf3d7b5f7dd5dbe5f67ae2d02ee407cf
ALI <ahmadalihusein@gmail.com>
2026-06-14 15:28:09 +0800

    Completed 02-data, 02

--- 02-data/Exercise02_Lists.java
+++ 02-data/Exercise02_Lists.java
@@ -11,6 +11,7 @@ public class Exercise02_Lists {
         tasks.add("Learn Java");
         tasks.add("Learn SQL");
         tasks.add("Get internship");
+       
 
         System.out.println("I have " + tasks.size() + " tasks:");
         for (String task : tasks) {
@@ -20,10 +21,27 @@ public class Exercise02_Lists {
         System.out.println("---");
 
         // TODO 1: add two more tasks of your own to the list, then print the list again.
+        tasks.add("Complete exercise 02");
+        tasks.add("Play Apex Legends");
+        System.out.println("After adding 2 tasks:");
+        for (String task : tasks){
+            System.out.println("- " + task);
+        }
+
+                System.out.println("---");
+
 
         // TODO 2: remove the first task (hint: tasks.remove(0)), then print the list.
+        tasks.remove( 0);
+        System.out.println("After removing first: ");
+        for (String task : tasks){
+            System.out.println("- " + task);
+        }
+        
+        System.out.println("---");
 
         // TODO 3: print whether the list contains "Learn SQL"
         //         (hint: tasks.contains("Learn SQL") gives true/false)
+        System.out.println("Has Learn SQL? " + tasks.contains("Learn SQL"));
     }
 }
